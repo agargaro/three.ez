13a879234e337b73a91f7006866ba8be5558f984
ANDREA GARGARO <itachi_955@hotmail.it>
2023-09-13 10:45:51 +0200

    Removed useless files

--- src/core/Main.ts
+++ src/core/Main.ts
@@ -1,15 +1,3 @@
-/**
- * This package exports all decorators that are available in Sequelize, built using the legacy stage-3 decorators proposal.
- *
- * All available decorators can be imported as follows:
- *
- * ```js
- * import { HasOne, Attribute } from '@sequelize/core/decorators-legacy';
- * ```
- *
- * @module Main-Module
- */
-
 import { Camera, Clock, ColorRepresentation, Raycaster, Scene, Vector2, WebGLRenderer, WebGLRendererParameters } from "three";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { Binding } from "../binding/Binding";
