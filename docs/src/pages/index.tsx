4e24661c3eecf29984d3ed232175348d55003309
ANDREA GARGARO <itachi_955@hotmail.it>
2023-09-13 10:50:33 +0200

    Removed useless import

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
