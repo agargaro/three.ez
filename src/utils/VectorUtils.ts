eea23bb55e6e7a26e035adc7c974d121ddbfc3f8
agargaro <devgargaro@gmail.com>
2023-10-21 22:22:48 +0200

    VectorUtils refactoring

--- src/utils/VectorUtils.ts
+++ src/utils/VectorUtils.ts
@@ -1,7 +1,7 @@
 import { Object3D, Vector2, Vector3 } from 'three';
 
 type VectorObject3D = Vector2 | Vector3 | Object3D;
-type Vector3Object3D = Vector3 | Object3D;
+type ObjVec3 = Vector3 | Object3D;
 
 const TEMP: Vector3[] = [...Array(4)].map(() => new Vector3());
 const ORIGIN = new Vector3();
@@ -25,11 +25,11 @@ export class VectorUtils {
     return Math.sign(TEMP[0].subVectors(point, origin).dot(normal));
   }
 
-  public static haveSameDirection(v1: Vector3, v2: Vector3, tolerance = 0.01): boolean {
+  public static haveSameDirection(v1: Vector3, v2: Vector3, tolerance = 10 ** -2): boolean {
     return TEMP[0].copy(v1).normalize().dot(TEMP[1].copy(v2).normalize()) > 1 - tolerance;
   }
 
-  public static haveOppositeDirection(v1: Vector3, v2: Vector3, tolerance = 0.01): boolean {
+  public static haveOppositeDirection(v1: Vector3, v2: Vector3, tolerance = 10 ** -2): boolean {
     return TEMP[0].copy(v1).normalize().dot(TEMP[1].copy(v2).normalize()) < tolerance - 1;
   }
 
@@ -42,29 +42,38 @@ export class VectorUtils {
     return this.computeSign(referencePoint, ORIGIN, target) !== 1 ? target : target.multiplyScalar(-1);
   }
 
-  public static perpendicularByPoints(p1: Vector3Object3D, p2: Vector3Object3D, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
+  public static perpendicularByPoints(p1: ObjVec3, p2: ObjVec3, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
     const [p1c, p2c] = this.getPositionsFromObject3D([p1, p2]);
     return target.crossVectors(TEMP[0].subVectors(p1c, p2c), normal);
   }
 
-  public static perpendicularByPointsSigned(p1: Vector3Object3D, p2: Vector3Object3D, referencePoint: Vector3Object3D, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
-    const [p1c, p2c, r] = this.getPositionsFromObject3D([p1, p2, referencePoint]);
+  public static perpendicularSignedByPoints(p1: ObjVec3, p2: ObjVec3, refPoint: ObjVec3, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
+    const [p1c, p2c, r] = this.getPositionsFromObject3D([p1, p2, refPoint]);
     target.crossVectors(TEMP[0].subVectors(p1c, p2c), normal);
     return this.computeSign(r, p1c, target) !== 1 ? target : target.multiplyScalar(-1);
   }
 
-  public static bisector(v1: Vector3, v2: Vector3, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
+  public static bisector(v1: Vector3, v2: Vector3, target = new Vector3()) {
     TEMP[0].copy(v1).normalize();
     TEMP[1].copy(v2).normalize();
     return target.addVectors(TEMP[0], TEMP[1]).normalize();
   }
 
-  public static bisectorByPoints(p1: Vector3Object3D, p2: Vector3Object3D, center: Vector3Object3D, target = new Vector3(), normal = this.DEFAULT_NORMAL) {
+  public static bisectorByPoints(p1: ObjVec3, p2: ObjVec3, center: ObjVec3, target = new Vector3()) {
     const [p1c, p2c, c] = this.getPositionsFromObject3D([p1, p2, center]);
-    return this.bisector(TEMP[2].subVectors(p1c, c), TEMP[3].subVectors(p2c, c), target, normal);
+    return this.bisector(TEMP[2].subVectors(p1c, c), TEMP[3].subVectors(p2c, c), target);
   }
 
-  public static arePointsOnSameSideByPoints(p1: Vector3Object3D, p2: Vector3Object3D, points: Vector3Object3D[], normal = this.DEFAULT_NORMAL): boolean {
+  public static arePointsOnSameSide(origin: ObjVec3, dir: Vector3, points: ObjVec3[]): boolean {
+    const [o, ...p] = this.getPositionsFromObject3D([origin, ...points]);
+    const sign = this.computeSign(p[0], o, dir);
+    for (let i = 1; i < points.length; i++) {
+      if (sign !== this.computeSign(p[i], o, dir)) return false;
+    }
+    return true;
+  }
+
+  public static arePointsOnSameSideByPoints(p1: ObjVec3, p2: ObjVec3, points: ObjVec3[], normal = this.DEFAULT_NORMAL): boolean {
     const [p1c, p2c, ...p] = this.getPositionsFromObject3D([p1, p2, ...points]);
     const dir = this.perpendicularByPoints(p1c, p2c, TEMP[0], normal);
     const sign = this.computeSign(p[0], p1c, dir);
@@ -74,28 +83,19 @@ export class VectorUtils {
     return true;
   }
 
-  public static arePointsOnSameSide(origin: Vector3Object3D, dir: Vector3, points: Vector3Object3D[], normal = this.DEFAULT_NORMAL): boolean {
-    const [o, ...p] = this.getPositionsFromObject3D([origin, ...points]);
-    const sign = this.computeSign(p[0], o, dir);
-    for (let i = 1; i < points.length; i++) {
-      if (sign !== this.computeSign(p[i], o, dir)) return false;
-    }
-    return true;
-  }
-
   // normal must be normalized
-  public static signedAngleFromOrigin(a: Vector3, b: Vector3, normal = this.DEFAULT_NORMAL): number {
+  public static angleSignedFromOrigin(a: Vector3, b: Vector3, normal = this.DEFAULT_NORMAL): number {
     return Math.atan2(TEMP[0].crossVectors(a, b).dot(normal), a.dot(b));
   }
 
-  public static signedAngleByPoints(p1: Vector3Object3D, p2: Vector3Object3D, center: Vector3Object3D, normal = this.DEFAULT_NORMAL): number {
+  public static angleSignedByPoints(p1: ObjVec3, p2: ObjVec3, center: ObjVec3, normal = this.DEFAULT_NORMAL): number {
     const [p1c, p2c, c] = this.getPositionsFromObject3D([p1, p2, center]);
     const a = TEMP[0].subVectors(p1c, c);
     const b = TEMP[1].subVectors(p2c, c);
     return Math.atan2(TEMP[2].crossVectors(a, b).dot(normal), a.dot(b));
   }
 
-  public static projectOnLine(point: Vector3Object3D, l1: Vector3Object3D, l2: Vector3Object3D, target = new Vector3()): Vector3 {
+  public static projectOnLine(point: ObjVec3, l1: ObjVec3, l2: ObjVec3, target = new Vector3()): Vector3 {
     const [vc, p1c, p2c] = this.getPositionsFromObject3D([point, l1, l2]);
     return target.subVectors(vc, p1c).projectOnVector(TEMP[0].subVectors(p1c, p2c)).add(p1c);
   }
@@ -121,15 +121,15 @@ export class VectorUtils {
   }
 
   // https://paulbourke.net/geometry/pointlineplane/
-  public static lineIntersection3D(p1: Vector3Object3D, p2: Vector3Object3D, p3: Vector3Object3D, p4: Vector3Object3D, target = new Vector3()): Vector3 {
+  public static lineIntersection3D(p1: ObjVec3, p2: ObjVec3, p3: ObjVec3, p4: ObjVec3, target = new Vector3(), tolerance = 10 ** -10): Vector3 {
     const [p1c, p2c, p3c, p4c] = this.getPositionsFromObject3D([p1, p2, p3, p4]);
 
     const p13 = TEMP[0].subVectors(p1c, p3c);
     const p43 = TEMP[1].subVectors(p4c, p3c);
 
-    if (p43.lengthSq() < Number.EPSILON) return;
+    if (p43.lengthSq() < tolerance) return;
     const p21 = TEMP[2].subVectors(p2c, p1c);
-    if (p21.lengthSq() < Number.EPSILON) return;
+    if (p21.lengthSq() < tolerance) return;
 
     const d1343 = p13.x * p43.x + p13.y * p43.y + p13.z * p43.z;
     const d4321 = p43.x * p21.x + p43.y * p21.y + p43.z * p21.z;
@@ -138,7 +138,7 @@ export class VectorUtils {
     const d2121 = p21.x * p21.x + p21.y * p21.y + p21.z * p21.z;
 
     const denom = d2121 * d4343 - d4321 * d4321;
-    if (Math.abs(denom) < Number.EPSILON) return;
+    if (Math.abs(denom) < tolerance) return;
 
     const numer = d1343 * d4321 - d1321 * d4343;
 
