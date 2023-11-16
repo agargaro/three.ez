563e3e0b058f80bc7a8c244be4321b206adcb88f
Luigi Denora <34819271+luigidenora@users.noreply.github.com>
2023-11-16 09:33:30 +0100

    Tween: Fix elapsedTime NaN when browser page is inactive (#59)

--- src/tweening/RunningTween.ts
+++ src/tweening/RunningTween.ts
@@ -275,7 +275,8 @@ export class RunningTween<T = any> {
     private executeActions(block: RunningBlock): void {
         if (block.actions) {
             for (const action of block.actions) {
-                const alpha = block.reversed ? 1 - Math.min(1, block.elapsedTime / action.time) : Math.min(1, block.elapsedTime / action.time);
+				const elapsedTime = Math.min(1, block.elapsedTime / (action.time + 10 ** -12));
+                const alpha = block.reversed ? 1 - elapsedTime : elapsedTime;
                 const easedAlpha = action.easing?.call(this, alpha) ?? alpha;
                 const applyValue = block.config?.onProgress?.call(this.target, this.target, action.key, action.start, action.end, easedAlpha);
                 if (applyValue !== false) {
