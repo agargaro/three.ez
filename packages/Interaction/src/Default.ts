d0ce631e3b1cc2c17e75964c5eb558dbbea0303c
agargaro <devgargaro@gmail.com>
2024-03-19 19:35:40 +0100

    wip

---
+++ packages/Interaction/src/Default.ts
@@ -0,0 +1,11 @@
+/**
+ * This class defines default settings for newly created Object3D instances.
+ */
+export class Default {
+    /** The default setting for 'focusable' for newly created Object3Ds. */
+    public static focusable = true;
+    /** The default setting for 'draggable' for newly created Object3Ds. */
+    public static draggable = false;
+    /** The default setting for 'interceptByRaycaster' for newly created Object3Ds. */
+    public static interceptByRaycaster = true;
+}
