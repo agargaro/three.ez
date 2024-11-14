73870151e81ded906db9fa0f57a6e7efea010db8
agargaro <devgargaro@gmail.com>
2024-11-14 18:59:03 +0100

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,25 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
-  {
-    files: ['**/*.{js,ts}'],
-  },
-  {
-    ignores: ['dist', 'vite.config.js', 'examples', 'docs', 'packages'],
-  },
   js.configs.recommended,
   ...tseslint.configs.strict,
+  stylistic.configs.customize({
+    jsx: false,
+    semi: true,
+    commaDangle: 'never',
+    arrowParens: true
+  }),
+  {
+    files: ['**/*.{js,ts}']
+  },
+  {
+    ignores: ['dist', 'vite.config.js', 'examples', 'docs', 'packages']
+  },
   {
     rules: {
+      'camelcase': 'warn',
       'no-unused-vars': 'off',
       'no-undef': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
@@ -19,6 +27,6 @@ export default [
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-wrapper-object-types': 'off',
       '@typescript-eslint/no-dynamic-delete': 'off'
-    },
-  },
+    }
+  }
 ];
