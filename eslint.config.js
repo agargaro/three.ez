3aa122807b7b2cad95940986a95f68222789bec5
Andrea Gargaro <andreagargaro_ca@hotmail.com>
2024-11-15 18:16:50 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       camelcase: 'warn',
       'no-unused-vars': 'off',
       'no-undef': 'off',
+      'prefer-rest-params': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
