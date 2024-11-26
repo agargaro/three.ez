e0581d3c6a7ef7551ebb439d0d78c00e5a4cec57
agargaro <devgargaro@gmail.com>
2024-11-26 22:48:09 +0100

    improve config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,41 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
-  {
-    files: ['**/*.{js,ts}'],
-  },
-  {
-    ignores: ['dist', 'vite.config.js', 'examples', 'test', 'docs', 'packages'],
-  },
   js.configs.recommended,
   ...tseslint.configs.strict,
+  stylistic.configs.customize({
+    jsx: false,
+    semi: true,
+    commaDangle: 'never',
+    arrowParens: true,
+    braceStyle: '1tbs',
+    blockSpacing: true,
+    indent: 2,
+    quoteProps: 'as-needed',
+    quotes: 'single'
+  }),
+  {
+    ignores: ['dist', 'examples', 'docs', 'packages']
+  },
   {
     rules: {
       'no-unused-vars': 'off',
       'no-undef': 'off',
+      'prefer-rest-params': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-wrapper-object-types': 'off'
-    },
-  },
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      '@typescript-eslint/no-dynamic-delete': 'off',
+      '@typescript-eslint/no-invalid-void-type': 'off',
+      '@typescript-eslint/no-this-alias': 'off',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error', {
+          allowExpressions: true
+        }
+      ]
+    }
+  }
 ];
